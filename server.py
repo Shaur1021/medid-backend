59d49001ea7cc8e2e3a16b7dd0701a4d5f7ef09a
Shaur1021 <monkeytype20@gmail.com>
2025-05-24 13:40:42 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+CORS(app, origins=["chrome-extension://cemkbgmnnmnalagobjeiohcaldkjjemc"])
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for extension use
